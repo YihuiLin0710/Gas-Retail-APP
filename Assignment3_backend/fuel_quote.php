8f5a79a0d345095322a3cbbb4a944838021b40a0
Lioncake <37010597+Yldx@users.noreply.github.com>
2022-03-13 23:46:16 -0500

    Update fuel_quote.php

--- Assignment3_backend/fuel_quote.php
+++ Assignment3_backend/fuel_quote.php
@@ -1,10 +1,63 @@
 <?php
     session_start();
-    $_SESSION['gallons'] = $_POST['gallons'];
-    $_SESSION['address'] = $_POST['address'];
-    $_SESSION['date']    = $_POST['date'];
-    $_SESSION['pricing_module'] = $_POST['pricing_module'];
-    $_SESSION['total']   = $_POST['total'];
+
+    class fuel_quote
+    {
+        public function setValues(){
+    
+            if (isset($_POST['gallons'])) {
+                $_SESSION['gallons'] = $_POST['gallons'];
+            }
+        else{
+            return false;
+		}
+        if (isset($_POST['address'])) {
+            $_SESSION['address'] = $_POST['address'];
+        }
+        else{
+            return false;
+		}
+        if (isset($_POST['date'])) {
+            $_SESSION['date'] = $_POST['date'];
+        }
+        else{
+            return false;
+		}
+        if (isset($_POST['pricing_module'])) {
+            $_SESSION['pricing_module'] = $_POST['pricing_module'];
+        }
+        else{
+            return false;
+		}
+        if (isset($_POST['total'])) {
+            $_SESSION['total'] = $_POST['total'];
+        }
+        else{
+            return false;
+		}
+    }
+    
+    if (isset($_POST['gallons'])) {
+        $_SESSION['gallons'] = $_POST['gallons'];
+    }
+    if (isset($_POST['address'])) {
+        $_SESSION['address'] = $_POST['address'];
+    }
+    if (isset($_POST['date'])) {
+        $_SESSION['date'] = $_POST['date'];
+    }
+    if (isset($_POST['pricing_module'])) {
+        $_SESSION['pricing_module'] = $_POST['pricing_module'];
+    }
+    if (isset($_POST['total'])) {
+        $_SESSION['total'] = $_POST['total'];
+    }
+
+    //$_SESSION['gallons'] = $_POST['gallons'];
+    //$_SESSION['address'] = $_POST['address'];
+    //$_SESSION['date']    = $_POST['date'];
+    //$_SESSION['pricing_module'] = $_POST['pricing_module'];
+    //$_SESSION['total']   = $_POST['total'];
     //echo $gallons, "<br>", $address, "<br>", $date, "<br>", $pricing_module, "<br>", $total;
 
     //Database connection
