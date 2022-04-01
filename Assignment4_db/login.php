3b2d5874a3a89fbec9fa63985464d4ba95337f94
Vincent Lin <70237957+YihuiLin0710@users.noreply.github.com>
2022-04-01 12:47:50 -0500

    Add files via upload

---
+++ Assignment4_db/login.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    $_SESSION['username'] = $_POST['user'];
+    $_SESSION['password'] = $_POST['password'];
+    $user = $_POST['user'];
+
+    $plaintext_password = $_POST['password'];
+    $hash = $_SESSION['hash'];
+    $verify = password_verify($plaintext_password, $hash);
+
+$T=new mysqli("localhost", "root","","profile");
+if($T->connect_error)
+    die("Fail to connect : ".$T->connect_error);
+else{
+    $stmt=$T->prepare("select * from customer_profile where username = ?");
+    $stmt->bind_param("s",$_SESSION['username']);
+    $stmt->execute();
+    $stmt_result=$stmt->get_result();
+    if($stmt_result->num_rows>0){
+        $data=$stmt_result->fetch_assoc();
+        if($verify){
+            echo "<h2> Login Successfully</h2>";
+            header("location:main.php");
+        }
+        else
+            echo "<h2> Invalid Username or Password</h2>";
+    }
+    else
+        echo "<h2> Invalid Username or Password</h2>";
+}
+
+?>
